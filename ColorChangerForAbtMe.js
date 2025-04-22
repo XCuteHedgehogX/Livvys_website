1855f19e6f5ec77d397029d847b45c7b6f9a6792
unknown <Livy>
2025-04-22 18:25:19 +0300

    PETS AND LAST LESSON =<

--- ColorChangerForAbtMe.js
+++ ColorChangerForAbtMe.js
@@ -1,83 +1,39 @@
-let ColorOne = document.querySelector (".Color1")
-let ColorTwo = document.querySelector (".Color2")
-let ColorThree = document.querySelector (".Color3")
-let ColorFour = document.querySelector (".Color4")
-ColorOne.addEventListener ("input",function(){
-console.log(this.value)
-let td = document.querySelectorAll ("td")
+let ColorOne = document.querySelector(".Color1");
+let ColorTwo = document.querySelector(".Color2");
+let ColorThree = document.querySelector(".Color3");
+let ColorFour = document.querySelector(".Color4");
+ColorOne.addEventListener("input", function () {
+  console.log(this.value);
+  let td = document.querySelectorAll("td");
 
-td.forEach(td1td => { 
-  td1td.style.borderColor=this.value
+  td.forEach((td1td) => {
+    td1td.style.borderColor = this.value;
+  });
 });
-}) 
-ColorTwo.addEventListener ("input",function(){
-    console.log(this.value)
-    let table = document.querySelectorAll ("table")
-    
-    table.forEach(td1td => { 
-      td1td.style.borderColor=this.value
-    });
-    }) 
-    ColorThree.addEventListener ("input",function(){
-        console.log(this.value)
-        let td = document.querySelectorAll ("td")
-        
-        td.forEach(td1td => { 
-          td1td.style.color=this.value
-        });
-        }) 
-        ColorFour.addEventListener ("input",function(){
-            console.log(this.value)
-            let th = document.querySelectorAll ("th")
-            
-            th.forEach(td1td => { 
-              td1td.style.color=this.value
-            });
-            }) 
+ColorTwo.addEventListener("input", function () {
+  console.log(this.value);
+  let table = document.querySelectorAll("table");
 
+  table.forEach((td1td) => {
+    td1td.style.borderColor = this.value;
+  });
+});
+ColorThree.addEventListener("input", function () {
+  console.log(this.value);
+  let td = document.querySelectorAll("td");
 
+  td.forEach((td1td) => {
+    td1td.style.color = this.value;
+  });
+});
+ColorFour.addEventListener("input", function () {
+  console.log(this.value);
+  let th = document.querySelectorAll("th");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  th.forEach((td1td) => {
+    td1td.style.
+    background = this.value;
+    console.log (td1td.style)
+    td1td.style.backgroundClip="text"
+  });
+});
